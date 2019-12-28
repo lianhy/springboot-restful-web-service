ffc466e3dbb4f4808fe4aae7c2620d5208f04f63
lianhy <lol_lianhy@163.com>
2019-12-28 15:05:12 +0800

    优化

--- src/main/java/cn/lianhy/demo/action/HelloWorld.java
+++ src/main/java/cn/lianhy/demo/action/HelloWorld.java
@@ -2,20 +2,14 @@ package cn.lianhy.demo.action;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/demo")
 public class HelloWorld {
 
-    @GetMapping(value = "sayHello.json")
-    public String sayHello(String userName){
-        log.info("Hello："+userName);
-        log.error("Hello："+userName);
-        log.warn("Hello："+userName);
-        log.debug("Hello："+userName);
-        return "Hello："+userName;
+    @GetMapping(value = "hello")
+    public String sayHello(){
+        return "Hello World!";
     }
 }
